4d4971e84b292c38ab3f35a79ae0c45281118ac5
Alan Price <kickflip720@gmail.com>
2011-11-12 14:45:27 +0000

    Added helloworld project via sourcetree

---
+++ Classes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  HelloWorld
+//
+//  Created by apple macbook on 12/11/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
